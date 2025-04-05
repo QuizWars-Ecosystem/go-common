78771c70d6bb527205b809d7bf2283c6354520ce
Nofre <davidprogram417@gmail.com>
2025-04-05 22:26:18 +0200

    added tern migrator testing helper

---
+++ pkg/testing/tern/tern.go
@@ -0,0 +1,22 @@
+package tern
+
+import (
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/tern/migrate"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func RunMigration(t *testing.T, connString, migrationPath string) {
+	conn, err := pgx.Connect(t.Context(), connString)
+	require.NoError(t, err)
+
+	migrator, err := migrate.NewMigrator(t.Context(), conn, "migrations")
+	require.NoError(t, err)
+
+	err = migrator.LoadMigrations(migrationPath)
+	require.NoError(t, err)
+
+	err = migrator.Migrate(t.Context())
+	require.NoError(t, err)
+}
