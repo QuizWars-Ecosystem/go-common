6ef101812859909e207ac78df66b3d97c795e9f6
Nofre <davidprogram417@gmail.com>
2025-04-16 14:51:06 +0200

    added helper for uuid pgx package

---
+++ pkg/uuidx/uuidx.go
@@ -0,0 +1,15 @@
+package uuidx
+
+import (
+	"github.com/gofrs/uuid"
+	pgxuuid "github.com/jackc/pgx/pgtype/ext/gofrs-uuid"
+)
+
+func NewUUIDFromString(id string) (pgxuuid.UUID, error) {
+	res, err := uuid.FromString(id)
+	if err != nil {
+		return pgxuuid.UUID{}, err
+	}
+
+	return pgxuuid.UUID{UUID: res}, nil
+}
