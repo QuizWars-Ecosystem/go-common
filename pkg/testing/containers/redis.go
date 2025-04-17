68400f12d6d54c2b48e132eeb6db90d8adca1876
Nofre <davidprogram417@gmail.com>
2025-04-18 00:23:05 +0200

    redis test container adjusted

--- pkg/testing/containers/redis.go
+++ pkg/testing/containers/redis.go
@@ -2,23 +2,15 @@ package containers
 
 import (
 	"context"
-	"time"
-
 	"github.com/QuizWars-Ecosystem/go-common/pkg/testing/config"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/redis"
-	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func NewRedisContainer(ctx context.Context, cfg *config.RedisConfig) (*redis.RedisContainer, error) {
 	return redis.Run(
 		ctx,
 		cfg.Image,
-		testcontainers.WithWaitStrategy(
-			wait.ForLog("Ready to accept connections").
-				WithOccurrence(2).
-				WithStartupTimeout(time.Second*5),
-		),
 		testcontainers.WithHostPortAccess(cfg.Ports...),
 	)
 }
