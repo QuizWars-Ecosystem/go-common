32d97e3e218c0f2d5d7c392176f5917eeacdaeaa
Nofre <davidprogram417@gmail.com>
2025-05-06 04:24:46 +0200

    chore: redis cluster test container updated

--- pkg/clients/redis_cluster.go
+++ pkg/clients/redis_cluster.go
@@ -24,7 +24,7 @@ var defaultClusterOptions = &redis.ClusterOptions{
 	RouteRandomly:   false,
 	PoolSize:        100,
 	MinIdleConns:    10,
-	DialTimeout:     5 * time.Second,
+	DialTimeout:     10 * time.Second,
 	ReadTimeout:     time.Second,
 	WriteTimeout:    time.Second,
 	MaxRetries:      5,
@@ -33,17 +33,17 @@ var defaultClusterOptions = &redis.ClusterOptions{
 }
 
 func NewRedisClusterOptions(addrs []string) *RedisClusterOptions {
-	opts := *defaultClusterOptions
+	opts := defaultClusterOptions
 	opts.Addrs = addrs
 	return &RedisClusterOptions{
-		ClusterOptions: &opts,
+		ClusterOptions: opts,
 	}
 }
 
-func NewRedisClusterClient(opts *RedisClusterOptions) (*redis.ClusterClient, error) {
+func NewRedisClusterClient(ctx context.Context, opts *RedisClusterOptions) (*redis.ClusterClient, error) {
 	client := redis.NewClusterClient(opts.ClusterOptions)
 
-	pingCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	pingCtx, cancel := context.WithTimeout(ctx, time.Second*15)
 	defer cancel()
 
 	if err := client.Ping(pingCtx).Err(); err != nil {
