87d661e498d528c20dea7ddfac658cc62da1b4c4
Nofre <davidprogram417@gmail.com>
2025-05-06 03:09:47 +0200

    feat: added new way to create redis as cluster in tests

--- pkg/testing/containers/redis.go
+++ pkg/testing/containers/redis.go
@@ -7,6 +7,7 @@ import (
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/redis"
 	"github.com/testcontainers/testcontainers-go/wait"
+	"os/exec"
 )
 
 func NewRedisContainer(ctx context.Context, cfg *config.RedisConfig) (*redis.RedisContainer, error) {
@@ -46,4 +47,65 @@ func NewRedisClusterContainers(ctx context.Context, cfg *config.RedisClusterConf
 	}
 
 	return containers, nil
+}
+
+func NewRedisClusterContainersV2(ctx context.Context, cfg *config.RedisClusterConfig) ([]testcontainers.Container, []string, error) {
+	var containers []testcontainers.Container
+	var containerURLs []string
+
+	for i := 0; i < cfg.ClusterSize; i++ {
+		req := testcontainers.ContainerRequest{
+			Image:        cfg.Image,
+			ExposedPorts: []string{"6379/tcp"},
+			WaitingFor:   wait.ForListeningPort("6379"),
+			Env: map[string]string{
+				"REDIS_CLUSTER": "yes",
+			},
+			Cmd: []string{
+				"redis-server",
+				"--port", "6379",
+				"--cluster-enabled", "yes",
+				"--cluster-config-file", fmt.Sprintf("nodes-%d.conf", i),
+				"--cluster-node-timeout", "5000",
+				"--appendonly", "yes",
+			},
+		}
+
+		container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+		})
+
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to start redis container: %w", err)
+		}
+
+		host, err := container.Host(ctx)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to get container host: %w", err)
+		}
+		port, err := container.MappedPort(ctx, "6379")
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to get container port: %w", err)
+		}
+
+		containerURLs = append(containerURLs, fmt.Sprintf("%s:%s", host, port.Port()))
+
+		containers = append(containers, container)
+	}
+
+	for i := 0; i < len(containerURLs); i++ {
+		for j := i + 1; j < len(containerURLs); j++ {
+			cmd := fmt.Sprintf(
+				"redis-cli --cluster add-node %s %s", containerURLs[j], containerURLs[i],
+			)
+			// Здесь запускаем команду для объединения Redis контейнеров в кластер
+			_, err := exec.Command("bash", "-c", cmd).Output()
+			if err != nil {
+				return nil, nil, fmt.Errorf("failed to add node to cluster: %w", err)
+			}
+		}
+	}
+
+	return containers, containerURLs, nil
 }
